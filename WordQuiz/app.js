1a1264ceb0f7265891e440b9e618fed54311e100
Uginim <kimugiugi@gmail.com>
2019-11-12 13:12:02 +0900

    feat: 단어퀴즈 페이지 작성

--- WordQuiz/app.js
+++ WordQuiz/app.js
@@ -1,20 +1,42 @@
 var express = require('express');
 var nunjucks = require('nunjucks');
 var app = express();
+const fs = require('fs');
+
 
 nunjucks.configure('views',{ 
     autoescape: true,
+    noCache: true,
     express:app
 });
 
 app.get('/',function(req, res, next){
     try{        
-        console.log("word.html");        
-        res.render('word.html',
-        {
-            word:"unary",
-            meanings:["단항의","이항의","삼항의"]
-        })
+        console.log("word.html");
+        // fs.open('./views/word.html','r',(err, fd)=>{
+        //     console.log(fd);
+        //     var buffer=new Int8Array(1024);;
+        //     fs.read(fd,buffer,0,1000,null,(err,byteread,buffer)=>{
+        //         console.log(buffer);
+        //     });
+        // });
+        fs.readFile('./datas/problems.json', 'utf8', (err,data) => {
+            const json = JSON.parse(data);        
+            json.problems.map((item,index)=>{                
+                item.answers=item.wrongAnswers.slice();
+                item.answers.push(item.correctAnswer);
+                for(let i = 0;i<item.answers.length;i++){
+                    let tmp = item.answers[i];
+                    const targetIdx = Math.floor(Math.random()*item.answers.length);
+                    item.answers[i] = item.answers[targetIdx];
+                    item.answers[targetIdx] = tmp;
+                }
+            });
+            console.log(json);
+            res.render('word.html',json); 
+            
+        });
+        
     } catch(error){
         console.error(error);
         next(error);
