c287ec49af3eaf9c224dbea9d84ef976bb145ce0
uginim <kimugiugi@gmail.com>
2019-11-18 01:45:07 +0900

    feat: Add a basic formatter

--- JSFormatter/formatter.js
+++ JSFormatter/formatter.js
@@ -1,3 +1,11 @@
-let formatter ={};
-
-module.exports = formatter;
+module.exports.print = (str, ...args) => {
+    for (var i = 0; i < args.length; i++) {
+        let re = new RegExp("\\{" + i + "\\}", "gm");     
+        console.log('re',re);   
+        str = str.replace(re, args[i]);
+        
+    }
+    
+    return str;
+    //var re = /{[0-9]*}/;
+};
---
+++ JSFormatter/test.js
@@ -0,0 +1,3 @@
+var formatter = require('./formatter.js');
+
+console.log(formatter.print('this is {0}와 {1} {2}',"aa","bb","cc"));
