9cc2872d5473376a03a15faf261c9c88ca858010
Uginim <kimugiugi@gmail.com>
2019-11-18 13:15:15 +0900

    chore

--- JSFormatter/formatter.js
+++ JSFormatter/formatter.js
@@ -1,3 +1,9 @@
+/**
+ * English: 
+ * Can be replaced with a template string
+ * Korean:
+ * 템플릿 문자열로 대체 가능
+ */
 module.exports.print = (str, ...args) => {
     for (var i = 0; i < args.length; i++) {
         let re = new RegExp("\\{" + i + "\\}", "gm");     
@@ -6,6 +12,17 @@ module.exports.print = (str, ...args) => {
         
     }
     
-    return str;
-    //var re = /{[0-9]*}/;
+    return str;    
 };
+/** 
+ * 
+ */
+module.exports.stringFormat = (str, ...args)=> {
+    
+    return str;
+}
+
+module.exports.dateFormat = (str, ...args)=> {
+    
+    return str;
+}
